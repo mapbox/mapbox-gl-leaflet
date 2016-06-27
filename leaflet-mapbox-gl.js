73df06a02b5f4c10d5e98c27ed90c56e93198de0
Vito Macchia <vito.macchia@gmail.com>
2016-06-27 12:48:45 +0200

    Added example using PruneCluster (https://github.com/SINTEF-9012/PruneCluster); Added closure and compatibility checks for leaflet 1.x; Added gitignore

--- leaflet-mapbox-gl.js
+++ leaflet-mapbox-gl.js
@@ -1,101 +1,112 @@
-L.MapboxGL = L.Class.extend({
+(function () {
+    var version = parseFloat(L.version);
+    var extend = version >= 1 ? L.Layer.extend.bind(L.Layer) : L.Class.extend.bind(L.Class);
+    L.MapboxGL = extend({
 
-    initialize: function (options) {
-        L.setOptions(this, options);
+        initialize: function (options) {
+            L.setOptions(this, options);
 
-        if (options.accessToken) {
-            mapboxgl.accessToken = options.accessToken;
-        } else {
-            throw new Error('You should provide a Mapbox GL access token as a token option.');
-        }
-    },
+            if (options.accessToken) {
+                mapboxgl.accessToken = options.accessToken;
+            } else {
+                throw new Error('You should provide a Mapbox GL access token as a token option.');
+            }
+        },
 
-    onAdd: function (map) {
-        this._map = map;
+        onAdd: function (map) {
+            this._map = map;
 
-        if (!this._glContainer) {
-            this._initContainer();
-        }
+            if (!this._glContainer) {
+                this._initContainer();
+            }
 
-        map._panes.tilePane.appendChild(this._glContainer);
-        map.on('zoomanim', this._animateZoom, this);
-        map.on('move', this._update, this);
+            map._panes.tilePane.appendChild(this._glContainer);
+            map.on('zoomanim', this._animateZoom, this);
+            map.on('move', this._update, this);
 
-        this._initGL();
-    },
+            this._initGL();
+        },
 
-    onRemove: function (map) {
-        map.getPanes().tilePane.removeChild(this._glContainer);
-        map.off('zoomanim', this._animateZoom, this);
-        map.off('move', this._update, this);
-        this._glMap.remove();
-        this._glMap = null;
-    },
+        onRemove: function (map) {
+            map.getPanes().tilePane.removeChild(this._glContainer);
+            map.off('zoomanim', this._animateZoom, this);
+            map.off('move', this._update, this);
+            this._glMap.remove();
+            this._glMap = null;
+        },
 
-    addTo: function (map) {
-        map.addLayer(this);
-        return this;
-    },
+        addTo: function (map) {
+            map.addLayer(this);
+            return this;
+        },
 
-    _initContainer: function () {
-        var container = this._glContainer = L.DomUtil.create('div', 'leaflet-gl-layer');
+        _initContainer: function () {
+            var container = this._glContainer = L.DomUtil.create('div', 'leaflet-gl-layer');
 
-        var size = this._map.getSize();
-        container.style.width  = size.x + 'px';
-        container.style.height = size.y + 'px';
-    },
-
-    _initGL: function () {
-        var center = this._map.getCenter();
-
-        var options = L.extend({}, this.options, {
-            container: this._glContainer,
-            interactive: false,
-            center: [center.lng, center.lat],
-            zoom: this._map.getZoom() - 1,
-            attributionControl: false
-        });
-
-        this._glMap = new mapboxgl.Map(options);
-        // allow GL base map to pan beyond min/max latitudes
-        this._glMap.transform.latRange = null;
-    },
-
-    _update: function () {
-        var size = this._map.getSize(),
-            container = this._glContainer,
-            gl = this._glMap,
-            topLeft = this._map.containerPointToLayerPoint([0, 0]);
-
-        L.DomUtil.setPosition(container, topLeft);
-        
-        var center = this._map.getCenter();
-
-        // gl.setView([center.lat, center.lng], this._map.getZoom() - 1, 0);
-        // calling setView directly causes sync issues because it uses requestAnimFrame
-
-        var tr = gl.transform;
-        tr.center = mapboxgl.LngLat.convert([center.lng, center.lat]);
-        tr.zoom = this._map.getZoom() - 1;
-
-        if (gl.transform.width !== size.x || gl.transform.height !== size.y) {
-            container.style.width  = size.x + 'px';
+            var size = this._map.getSize();
+            container.style.width = size.x + 'px';
             container.style.height = size.y + 'px';
-            gl.resize();
-        } else {
-            gl._update();
+        },
+
+        _initGL: function () {
+            var center = this._map.getCenter();
+
+            var options = L.extend({}, this.options, {
+                container: this._glContainer,
+                interactive: false,
+                center: [center.lng, center.lat],
+                zoom: this._map.getZoom() - 1,
+                attributionControl: false
+            });
+
+            this._glMap = new mapboxgl.Map(options);
+            // allow GL base map to pan beyond min/max latitudes
+            this._glMap.transform.latRange = null;
+        },
+
+        _update: function () {
+            var size = this._map.getSize(),
+                container = this._glContainer,
+                gl = this._glMap,
+                topLeft = this._map.containerPointToLayerPoint([0, 0]);
+
+            L.DomUtil.setPosition(container, topLeft);
+
+            var center = this._map.getCenter();
+
+            // gl.setView([center.lat, center.lng], this._map.getZoom() - 1, 0);
+            // calling setView directly causes sync issues because it uses requestAnimFrame
+
+            var tr = gl.transform;
+            tr.center = mapboxgl.LngLat.convert([center.lng, center.lat]);
+            tr.zoom = this._map.getZoom() - 1;
+
+            if (tr.width !== size.x || tr.height !== size.y) {
+                container.style.width = size.x + 'px';
+                container.style.height = size.y + 'px';
+                gl.resize();
+            } else {
+                gl._update();
+            }
+        },
+
+        _animateZoom: function (e) {
+            var origin;
+            if (version < 1) {
+                origin = e.origin.add(this._map._getMapPanePos()).subtract(this._map.getSize().divideBy(2));
+            }
+            else {
+                origin = this._map.latLngToLayerPoint(e.center).subtract(this._map.getSize().divideBy(2));
+            }
+
+            this._glMap.zoomTo(e.zoom - 1, {
+                duration: 250,
+                offset: [origin.x, origin.y]
+            });
         }
-    },
+    });
 
-    _animateZoom: function (e) {
-        var origin = e.origin.add(this._map._getMapPanePos()).subtract(this._map.getSize().divideBy(2));
-        this._glMap.zoomTo(e.zoom - 1, {
-            duration: 250,
-            offset: [origin.x, origin.y]
-        });
-    }
-});
-
-L.mapboxGL = function (options) {
-    return new L.MapboxGL(options);
-};
+    L.mapboxGL = function (options) {
+        return new L.MapboxGL(options);
+    };
+})();
