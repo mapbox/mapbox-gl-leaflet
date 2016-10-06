7212b44986bde957f28a1df4e0869e21280f6d0f
Patrick Arlt <patrick.arlt@gmail.com>
2016-10-06 14:46:43 -0700

    remove duplicate throttle function

--- leaflet-mapbox-gl.js
+++ leaflet-mapbox-gl.js
@@ -1,31 +1,3 @@
-function throttle(fn, time, context) {
-    var lock, args, wrapperFn, later;
-
-    later = function () {
-        // reset lock and call if queued
-        lock = false;
-        if (args) {
-            wrapperFn.apply(context, args);
-            args = false;
-        }
-    };
-
-    wrapperFn = function () {
-        if (lock) {
-            // called too soon, queue to call later
-            args = arguments;
-
-        } else {
-            // call and lock until later
-            fn.apply(context, arguments);
-            setTimeout(later, time);
-            lock = true;
-        }
-    };
-
-    return wrapperFn;
-};
-
 L.MapboxGL = L.Layer.extend({
     options: {
       updateInterval: 32
